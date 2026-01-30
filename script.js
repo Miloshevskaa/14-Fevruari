b2940b2642c730e654d4a19ce25a42f2f78a6a99
Miloshevskaa <milamiloshevskaa@gmail.com>
2026-01-30 13:24:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const message = document.getElementById("message");
+
+let noClicks = 0;
+
+noBtn.addEventListener("click", () => {
+    noClicks++;
+
+    if (noClicks === 1) {
+        noBtn.textContent = "Are you sure? 😢";
+    } else if (noClicks === 2) {
+        noBtn.textContent = "Please? 🥺";
+    } else if (noClicks === 3) {
+        noBtn.textContent = "Last chance 😭";
+    } else {
+        moveNoButton();
+    }
+});
+
+function moveNoButton() {
+    const x = Math.random() * 300 - 150;
+    const y = Math.random() * 300 - 150;
+    noBtn.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+yesBtn.addEventListener("click", () => {
+    message.textContent = "YAYYY!! 💖💖💖 I knew it!";
+    yesBtn.style.display = "none";
+    noBtn.style.display = "none";
+});
